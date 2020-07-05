cf75f96b23c750f1af95eb7b13426749863e18b8
erwin ardias <erwin.ardias@gmail.com>
2020-07-05 11:17:43 +0700

    auto login

--- app/src/main/java/com/example/petme/MainActivity.kt
+++ app/src/main/java/com/example/petme/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.petme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null){
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         login_hp.setOnClickListener {
             val intent = Intent(this, HandphoneLoginActivity::class.java)
